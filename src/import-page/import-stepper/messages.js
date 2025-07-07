350ff94e1bb84635ea2c0c7b332b167da3d7036f
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-07 20:05:56 +0300

    перевод сообщений

--- src/import-page/import-stepper/messages.js
+++ src/import-page/import-stepper/messages.js
@@ -3,55 +3,55 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   stepperUploadingTitle: {
     id: 'course-authoring.import.stepper.title.uploading',
-    defaultMessage: 'Uploading',
+    defaultMessage: 'Загрузка',
   },
   stepperUnpackingTitle: {
     id: 'course-authoring.import.stepper.title.unpacking',
-    defaultMessage: 'Unpacking',
+    defaultMessage: 'Распаковка',
   },
   stepperVerifyingTitle: {
     id: 'course-authoring.import.stepper.title.verifying',
-    defaultMessage: 'Verifying',
+    defaultMessage: 'Проверка',
   },
   stepperUpdatingTitle: {
     id: 'course-authoring.import.stepper.title.updating',
-    defaultMessage: 'Updating сourse',
+    defaultMessage: 'Обновление курса',
   },
   stepperSuccessTitle: {
     id: 'course-authoring.import.stepper.title.success',
-    defaultMessage: 'Success',
+    defaultMessage: 'Успех',
   },
   stepperUploadingDescription: {
     id: 'course-authoring.import.stepper.description.uploading',
-    defaultMessage: 'Transferring your file to our servers',
+    defaultMessage: 'Передача вашего файла на наши серверы',
   },
   stepperUnpackingDescription: {
     id: 'course-authoring.import.stepper.description.unpacking',
-    defaultMessage: 'Expanding and preparing folder/file structure (You can now leave this page safely, but avoid making drastic changes to content until this import is complete)',
+    defaultMessage: 'Распаковка и подготовка структуры папок/файлов (Вы можете безопасно покинуть эту страницу, но избегайте внесения значительных изменений в содержимое до завершения импорта)',
   },
   stepperVerifyingDescription: {
     id: 'course-authoring.import.stepper.description.verifying',
-    defaultMessage: 'Reviewing semantics, syntax, and required data',
+    defaultMessage: 'Проверка семантики, синтаксиса и обязательных данных',
   },
   stepperUpdatingDescription: {
     id: 'course-authoring.import.stepper.description.updating',
-    defaultMessage: 'Integrating your imported content into this course. This process might take longer with larger courses.',
+    defaultMessage: 'Интеграция импортированного содержимого в этот курс. Этот процесс может занять больше времени для более крупных курсов.',
   },
   stepperSuccessDescription: {
     id: 'course-authoring.import.stepper.description.success',
-    defaultMessage: 'Your imported content has now been integrated into this course',
+    defaultMessage: 'Импортированное содержимое теперь интегрировано в этот курс',
   },
   viewOutlineButton: {
     id: 'course-authoring.import.stepper.button.outline',
-    defaultMessage: 'View updated outline',
+    defaultMessage: 'Просмотреть обновленный план',
   },
   defaultErrorMessage: {
     id: 'course-authoring.import.stepper.error.default',
-    defaultMessage: 'Error importing course',
+    defaultMessage: 'Ошибка импорта курса',
   },
   stepperHeaderTitle: {
     id: 'course-authoring.import.stepper.header.title',
-    defaultMessage: 'Course import status',
+    defaultMessage: 'Статус импорта курса',
   },
 });
 
