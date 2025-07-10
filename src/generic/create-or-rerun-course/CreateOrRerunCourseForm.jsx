0d417e4d598d1f745ebcb2d829847a3fe6592fe0
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-10 16:22:28 +0300

    пох

--- src/generic/create-or-rerun-course/CreateOrRerunCourseForm.jsx
+++ src/generic/create-or-rerun-course/CreateOrRerunCourseForm.jsx
@@ -110,38 +110,36 @@ const CreateOrRerunCourseForm = ({
   };
 
   const renderOrgField = (field) => (allowToCreateNewOrg ? (
-    // <TypeaheadDropdown
-    //   readOnly={false}
-    //   name={field.name}
-    //   value={field.value}
-    //   controlClassName={classNames({ 'is-invalid': hasErrorField(field.name) })}
-    //   options={field.options}
-    //   placeholder={field.placeholder}
-    //   handleBlur={handleCustomBlurForDropdown}
-    //   handleChange={(value) => setFieldValue(field.name, value)}
-    //   noOptionsMessage={messages.courseOrgNoOptions.defaultMessage}
-    //   helpMessage=""
-    //   errorMessage=""
-    //   floatingLabel=""
-    // />
-    null
+    <TypeaheadDropdown
+      readOnly={false}
+      name={field.name}
+      value={field.value}
+      controlClassName={classNames({ 'is-invalid': hasErrorField(field.name) })}
+      options={field.options}
+      placeholder={field.placeholder}
+      handleBlur={handleCustomBlurForDropdown}
+      handleChange={(value) => setFieldValue(field.name, value)}
+      noOptionsMessage={messages.courseOrgNoOptions.defaultMessage}
+      helpMessage=""
+      errorMessage=""
+      floatingLabel=""
+    />
   ) : (
-    null
-    // <Dropdown className="mr-2">
-    //   <Dropdown.Toggle id={`${field.name}-dropdown`} variant="outline-primary">
-    //     {field.value || messages.courseOrgNoOptions.defaultMessage}
-    //   </Dropdown.Toggle>
-    //   <Dropdown.Menu>
-    //     {field.options?.map((value) => (
-    //       <Dropdown.Item
-    //         key={value}
-    //         onClick={() => setFieldValue(field.name, value)}
-    //       >
-    //         {value}
-    //       </Dropdown.Item>
-    //     ))}
-    //   </Dropdown.Menu>
-    // </Dropdown>
+    <Dropdown className="mr-2">
+      <Dropdown.Toggle id={`${field.name}-dropdown`} variant="outline-primary">
+        {field.value || messages.courseOrgNoOptions.defaultMessage}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {field.options?.map((value) => (
+          <Dropdown.Item
+            key={value}
+            onClick={() => setFieldValue(field.name, value)}
+          >
+            {value}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
   ));
 
   useEffect(() => {
