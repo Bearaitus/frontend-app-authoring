cfffbab399cfeabc5150f624fc08b8a16b96d897
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-10 16:05:34 +0300

    test2

--- src/generic/create-or-rerun-course/CreateOrRerunCourseForm.jsx
+++ src/generic/create-or-rerun-course/CreateOrRerunCourseForm.jsx
@@ -61,7 +61,7 @@ const CreateOrRerunCourseForm = ({
     {
       label: messages.courseOrgLabel.defaultMessage,
       name: 'org',
-      value: "Positive Technologies",
+      value: "Positive_Technologies",
       options: organizations,
       placeholder: messages.courseOrgPlaceholder.defaultMessage,
       disabled: false,
