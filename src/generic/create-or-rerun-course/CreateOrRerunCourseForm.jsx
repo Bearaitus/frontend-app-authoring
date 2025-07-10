3f605a84d16c3400980193dfe1e1544a1403bf99
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-10 15:59:22 +0300

    откат

--- src/generic/create-or-rerun-course/CreateOrRerunCourseForm.jsx
+++ src/generic/create-or-rerun-course/CreateOrRerunCourseForm.jsx
@@ -62,6 +62,7 @@ const CreateOrRerunCourseForm = ({
       label: messages.courseOrgLabel.defaultMessage,
       name: 'org',
       value: values.org,
+      options: organizations,
       placeholder: messages.courseOrgPlaceholder.defaultMessage,
       disabled: false,
     },
