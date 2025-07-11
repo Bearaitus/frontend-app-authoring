73b4e84a67ef181d503db2ee0e22998d4958a24b
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-11 12:28:20 +0300

    ок

--- src/import-page/file-section/messages.js
+++ src/import-page/file-section/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   headingTitle: {
     id: 'course-authoring.import.file-section.title',
-    defaultMessage: 'Выберите .tar.gz для импортирования курса',
+    defaultMessage: 'Выберите архив (.tar.gz) импортируемого курса',
   },
   fileChosen: {
     id: 'course-authoring.import.file-section.chosen-file',
