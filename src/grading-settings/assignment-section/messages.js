b9142a89fd029aadf5c048b28f9a4d5170045947
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-11 15:00:05 +0300

    исравление орфографической ошибки

--- src/grading-settings/assignment-section/messages.js
+++ src/grading-settings/assignment-section/messages.js
@@ -55,7 +55,7 @@ const messages = defineMessages({
   },
   numberOfDroppableTitle: {
     id: 'course-authoring.grading-settings.assignment.number-of-droppable.title',
-    defaultMessage: 'Рассчётный параметр',
+    defaultMessage: 'Расчётный параметр',
   },
   numberOfDroppableDescription: {
     id: 'course-authoring.grading-settings.assignment.number-of-droppable.description',
