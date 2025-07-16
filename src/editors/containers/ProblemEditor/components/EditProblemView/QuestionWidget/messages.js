dedce7ca9ead03e3023a516783ee13d826181429
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-16 16:31:10 +0300

    перевод

--- src/editors/containers/ProblemEditor/components/EditProblemView/QuestionWidget/messages.js
+++ src/editors/containers/ProblemEditor/components/EditProblemView/QuestionWidget/messages.js
@@ -4,12 +4,12 @@ const messages = defineMessages({
 
   questionWidgetTitle: {
     id: 'authoring.questionwidget.question.questionWidgetTitle',
-    defaultMessage: 'Question',
+    defaultMessage: 'Вопрос',
     description: 'Question Title',
   },
   placeholder: {
     id: 'authoring.problemEditor.questionwidget.placeholder',
-    defaultMessage: 'Enter your question',
+    defaultMessage: 'Поле ввода',
     description: 'Placeholder text for tinyMCE editor',
   },
 });
