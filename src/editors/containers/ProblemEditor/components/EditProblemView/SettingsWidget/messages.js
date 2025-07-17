19105277d753cfafabf184b32d9800b49c053811
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-17 12:53:44 +0300

    балл

--- src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/messages.js
+++ src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/messages.js
@@ -99,7 +99,7 @@ const messages = defineMessages({
   },
   weightSummary: {
     id: 'authoring.problemeditor.settings.scoring.weight',
-    defaultMessage: '{weight, plural, =1 other {# балла}}',
+    defaultMessage: '{weight, plural, =1 {балл} other {# балла}}',
     description: 'Текст суммирования для веса оценки',
   },
   scoringSettingsLabel: {
