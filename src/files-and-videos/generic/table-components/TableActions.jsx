7cc32f6c422b3ef89d8f9a60cfa1eaa513443a80
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-18 14:26:22 +0300

    Удаление кнопки действия и нормальная надпись

--- src/files-and-videos/generic/table-components/TableActions.jsx
+++ src/files-and-videos/generic/table-components/TableActions.jsx
@@ -38,41 +38,8 @@ const TableActions = ({
       <Button variant="outline-primary" onClick={openSort} iconBefore={Tune}>
         <FormattedMessage {...messages.sortButtonLabel} />
       </Button>
-      <Dropdown className="mx-2">
-        <Dropdown.Toggle
-          id="actions-menu-toggle"
-          alt="actions-menu-toggle"
-          variant="outline-primary"
-        >
-          <FormattedMessage {...messages.actionsButtonLabel} />
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {encodingsDownloadUrl ? (
-            <Dropdown.Item
-              download
-              href={`${getConfig().STUDIO_BASE_URL}${encodingsDownloadUrl}`}
-            >
-              <FormattedMessage {...messages.downloadEncodingsTitle} />
-            </Dropdown.Item>
-          ) : null}
-          <Dropdown.Item
-            onClick={() => handleBulkDownload(selectedFlatRows)}
-            disabled={_.isEmpty(selectedFlatRows)}
-          >
-            <FormattedMessage {...messages.downloadTitle} />
-          </Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item
-            data-testid="open-delete-confirmation-button"
-            onClick={() => handleOpenDeleteConfirmation(selectedFlatRows)}
-            disabled={_.isEmpty(selectedFlatRows)}
-          >
-            <FormattedMessage {...messages.deleteTitle} />
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
       <Button iconBefore={Add} onClick={fileInputControl.click}>
-        {intl.formatMessage(messages.addFilesButtonLabel, { fileType })}
+        Добавить файл
       </Button>
       <SortAndFilterModal {...{ isSortOpen, closeSort, handleSort }} />
     </>
