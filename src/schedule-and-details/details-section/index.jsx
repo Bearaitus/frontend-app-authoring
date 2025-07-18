d637cb443cbf165e7c8b65024bffb8a0b1ba64cf
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-18 20:28:51 +0300

    детали курса удалены

--- src/schedule-and-details/details-section/index.jsx
+++ src/schedule-and-details/details-section/index.jsx
@@ -15,34 +15,7 @@ const DetailsSection = ({
     return result ? result[1] : messages.dropdownEmpty.defaultMessage;
   };
 
-  return (
-    <section className="section-container details-section">
-      <SectionSubHeader
-        title={messages.detailsTitle.defaultMessage}
-        description={messages.detailsDescription.defaultMessage}
-      />
-      <Form.Group className="form-group-custom dropdown-language">
-        <Form.Label>{messages.dropdownLabel.defaultMessage}</Form.Label>
-        <Dropdown className="bg-white">
-          <Dropdown.Toggle variant="outline-primary" id="languageDropdown">
-            {formattedLanguage()}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            {languageOptions.map((option) => (
-              <Dropdown.Item
-                key={option[0]}
-                onClick={() => onChange(option[0], 'language')}
-              >
-                {option[1]}
-              </Dropdown.Item>
-            ))}
-          </Dropdown.Menu>
-        </Dropdown>
-        <Form.Control.Feedback>
-          {messages.dropdownHelpText.defaultMessage}
-        </Form.Control.Feedback>
-      </Form.Group>
-    </section>
+  return (null
   );
 };
 
