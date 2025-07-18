b0332bdee70f4a25b84d5c5575b5676a46934ce8
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-18 20:44:21 +0300

    видео интродукция нахуй

--- src/schedule-and-details/introducing-section/introduction-video/index.jsx
+++ src/schedule-and-details/introducing-section/introduction-video/index.jsx
@@ -11,43 +11,7 @@ const IntroductionVideo = ({ intl, introVideo, onChange }) => {
     : '';
 
   return (
-    <Form.Group className="form-group-custom">
-      <Form.Label>
-        {messages.courseIntroductionVideoLabel.defaultMessage}
-      </Form.Label>
-      <Card>
-        <Card.Body className="embed-video-container">
-          <div className="introduction-video">
-            <iframe
-              title={messages.courseIntroductionVideoLabel.defaultMessage}
-              width="618"
-              height="350"
-              src={embedVideoUrl}
-              frameBorder="0"
-              allowFullScreen
-            />
-          </div>
-        </Card.Body>
-        <Card.Divider />
-        <Card.Footer className="p-2.5">
-          <Form.Control
-            value={introVideo || ''}
-            placeholder={messages.courseIntroductionVideoPlaceholder.defaultMessage}
-            onChange={(e) => onChange(e.target.value, 'introVideo')}
-          />
-          <Button
-            variant="outline-primary"
-            onClick={() => onChange('', 'introVideo')}
-            disabled={!introVideo}
-          >
-            {messages.courseIntroductionVideoDelete.defaultMessage}
-          </Button>
-        </Card.Footer>
-      </Card>
-      <Form.Control.Feedback>
-        {messages.courseIntroductionVideoHelpText.defaultMessage}
-      </Form.Control.Feedback>
-    </Form.Group>
+null
   );
 };
 
