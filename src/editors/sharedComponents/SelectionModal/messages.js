496668d45db0139aaa0177a18334cf688d78f163
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-18 14:59:15 +0300

    Вот так

--- src/editors/sharedComponents/SelectionModal/messages.js
+++ src/editors/sharedComponents/SelectionModal/messages.js
@@ -1,34 +1,33 @@
 const messages = {
   searchPlaceholder: {
     id: 'authoring.selectionmodal.search.placeholder',
-    defaultMessage: 'Search',
+    defaultMessage: 'Поиск',
     description: 'Placeholder text for search bar',
   },
   clearSearch: {
     id: 'authoring.selectionmodal.search.clearSearchButton',
-    defaultMessage: 'Clear search query',
+    defaultMessage: 'Очистить запрос',
     description: 'Button to clear search query',
   },
   emptySearchLabel: {
     id: 'authoring.selectionmodal.emptySearchLabel',
-    defaultMessage: 'No search results.',
+    defaultMessage: 'По запросу ничего не найдено.',
     description: 'Label for when search returns nothing.',
   },
   loading: {
     id: 'authoring.selectionmodal.spinner.readertext',
-    defaultMessage: 'loading...',
+    defaultMessage: 'Загрузка...',
     description: 'Gallery loading spinner screen-reader text',
   },
   addedDate: {
     id: 'authoring.selectionmodal.addedDate.label',
-    defaultMessage: 'Added {date} at {time}',
+    defaultMessage: 'Добавлено {date} в {time}',
     description: 'File date-added string',
   },
   sortBy: {
     id: 'authoring.selectionmodal.sortBy',
-    defaultMessage: 'By',
+    defaultMessage: 'Сортировка по',
     description: '"By" before sort option',
   },
 };
-
 export default messages;
