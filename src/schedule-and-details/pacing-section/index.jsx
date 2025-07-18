6389e1711e640aa55fb41fe3d7d65a2f2d1c8af0
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-18 19:38:58 +0300

    Темп курса покинул колоду

--- src/schedule-and-details/pacing-section/index.jsx
+++ src/schedule-and-details/pacing-section/index.jsx
@@ -12,40 +12,7 @@ const PacingSection = ({
   const intl = useIntl();
   const canTogglePace = new Date() <= new Date(startDate);
 
-  return (
-    <section className="section-container pacing-section">
-      <SectionSubHeader
-        title={messages.pacingTitle.defaultMessage}
-        description={messages.pacingDescription.defaultMessage}
-      />
-      <Form.Group>
-        {!canTogglePace && (
-          <Form.Label className="text pt-3">
-            {messages.pacingRestriction.defaultMessage}
-          </Form.Label>
-        )}
-        <Form.RadioSet
-          name="selfPaced"
-          onChange={(e) => onChange(e.target.value, e.target.name)}
-          value={selfPaced.toString()}
-        >
-          <Form.Radio
-            value="false"
-            description={messages.pacingTypeInstructorDescription.defaultMessage}
-            disabled={!canTogglePace}
-          >
-            {messages.pacingTypeInstructorLabel.defaultMessage}
-          </Form.Radio>
-          <Form.Radio
-            value="true"
-            description={messages.pacingTypeSelfDescription.defaultMessage}
-            disabled={!canTogglePace}
-          >
-            {messages.pacingTypeSelfLabel.defaultMessage}
-          </Form.Radio>
-        </Form.RadioSet>
-      </Form.Group>
-    </section>
+  return (null
   );
 };
 
