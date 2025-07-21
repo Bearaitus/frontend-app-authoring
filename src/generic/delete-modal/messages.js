ae62477dc4f32af0183ae0445761e67f9433b3e7
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-21 20:33:46 +0300

    dkkd

--- src/generic/delete-modal/messages.js
+++ src/generic/delete-modal/messages.js
@@ -2,11 +2,11 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   title: {
     id: 'course-authoring.course-outline.delete-modal.title',
-    defaultMessage: 'Delete this {category}?',
+    defaultMessage: 'Delete?',
   },
   description: {
     id: 'course-authoring.course-outline.delete-modal.description',
-    defaultMessage: 'Deleting this {category} is permanent and cannot be undone.',
+    defaultMessage: 'Deleting is permanent and cannot be undone.',
   },
   deleteButton: {
     id: 'course-authoring.course-outline.delete-modal.button.delete',
